23130fd3dafee851e625b3d262274198c1e76140
diegodev <andreelopez334@gmail.com>
2023-11-29 23:14:56 -0600

    updates

--- Routes/ReporteRoutes.js
+++ Routes/ReporteRoutes.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router();
 const ReporteController = require('../Controllers/ReporteController')
 router.get('/reporte', ReporteController.Get)
-roouter.post('/reporte', ReporteController.Post)
+router.post('/reporte', ReporteController.Post)
 module.exports = router;
